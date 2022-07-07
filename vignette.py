fdab601e27f13543ec662cab71f77813a2f02ead
YapZanan <naufalsetiawan64@gmail.com>
2022-07-07 08:43:39 +0700

    tambahan

--- vignette.py
+++ vignette.py
@@ -1,184 +1,134 @@
-import cv2
-import sys
-import os
-import math
 import numpy as np
-from scipy.ndimage import gaussian_filter1d
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
 
+def log_entropy(image, sigma_smooth=2.25):
+  N = 256
+  n_hist = np.zeros(N, dtype=float)
 
-def check_monotonically_increase(parameter_tup):
-    """Check if a, b, c could let g(r) monotonically increase in [0,1]"""
+  rescaled_image = (N - 1) * (image / image.max())
+  li = (N - 1) * np.log2(1 + rescaled_image) / np.log2(N)
+  li_floor = np.floor(li).astype(int)
+  li_ceil = np.ceil(li).astype(int)
 
-    a, b, c = parameter_tup
-    if c == 0:
-        if a >= 0 and a + 2 * b >= 0 and not(a == 0 and b == 0):
-            return True
-        return False
-    if c < 0:
-        if b**2 > 3 * a * c:
-            q_plus = (-2 * b + math.sqrt(4 * b**2 - 12 * a * c)) / (6 * c)
-            q_minus = (-2 * b - math.sqrt(4 * b**2 - 12 * a * c)) / (6 * c)
-            if q_plus <= 0 and q_minus >= 1:
-                return True
-        return False
-    if c > 0:
-        if b**2 < 3 * a * c:
-            return True
-        elif b**2 == 3 * a * c:
-            if b >= 0 or 3 * c + b <= 0:
-                return True
-        else:
-            q_plus = (-2 * b + math.sqrt(4 * b**2 - 12 * a * c)) / (6 * c)
-            q_minus = (-2 * b - math.sqrt(4 * b**2 - 12 * a * c)) / (6 * c)
-            if q_plus <= 0 or q_minus >= 1:
-                return True
-        return False
+  np.add.at(n_hist, li_floor, 1 + li_floor - li)
+  np.add.at(n_hist, li_ceil, li_ceil - li)
 
+  n_hist_smooth = gaussian_filter(n_hist, sigma_smooth)
 
-def calc_discrete_entropy(cm_x, cm_y, max_distance, parameter_tup, im):
-    """
-    Calculate the discrete entropy after the brightness of the picture is
-    adjusted by the gain function with given parameters
-    """
+  pk = n_hist_smooth / np.sum(n_hist_smooth)
+  log_pk = np.where(pk != 0, np.log2(pk), 0)
 
-    print(parameter_tup)
+  return - np.sum(pk * log_pk)
 
-    a, b, c = parameter_tup
-    row, col = im.shape
+def compute_center_of_mass(image):
+  (sum, i_sum, j_sum) = (0, 0, 0)
 
-    histogram = [0 for i in range(256)]
+  for j in range(image.shape[0]):
+    for i in range(image.shape[1]):
+      sum += image[j, i]
 
-    for i in range(col):
-        for j in range(row):
+      i_sum += (i + 1) * image[j, i]
+      j_sum += (j + 1) * image[j, i]
+  return (i_sum / sum, j_sum / sum)
 
-            # calculate the distance from the current pixel to picture's center of mass and the corresponding r value
-            distance = math.sqrt((i - cm_x)**2 + (j - cm_y)**2)
-            r = distance / max_distance
+def compute_r_matrix(image):
+  (i_mid, j_mid) = compute_center_of_mass(image)
 
-            # evaluate the gain function and adjust pixel luminance value
-            g = 1 + a * r**2 + b * r**4 + c * r**6
-            intensity = im[j, i] * g
+  res = np.empty_like(image, dtype=float)
 
-            # map the luminance value to the corresponding histogram bins
-            bin = 255 * math.log(1 + intensity) / math.log(256)
-            floor_bin = math.floor(bin)
-            ceil_bin = math.ceil(bin)
+  d = np.sqrt(i_mid**2 + j_mid**2)
 
-            # if the luminance value exceeds 255 after adjustion, simply add histogram bins at the upper end
-            if bin > len(histogram) - 1:
-                for k in range(len(histogram), ceil_bin + 1):
-                    histogram.append(0)
+  for j in range(image.shape[0]):
+    for i in range(image.shape[1]):
+      res[j, i] = np.sqrt((i - i_mid)**2 + (j - j_mid)**2) / d
 
-            histogram[floor_bin] += 1 + floor_bin - bin
-            histogram[ceil_bin] += ceil_bin - bin
+  return res
 
-    # Use Gausssian kernel to smooth the histogram
-    histogram = gaussian_filter1d(histogram, 4)
+def g(r, a, b, c):
+  return 1 + a * r**2 + b * r**4 + c * r**6
 
-    histogram_sum = sum(histogram)
-    H = 0
+def verify_constraints(a, b, c):
+  if a > 0 and b == 0 and c == 0:
+      return True
+  if a >= 0 and b > 0 and c == 0:
+      return True
+  if c == 0 and b < 0 and -a <= 2 * b:
+      return True
+  if c > 0 and b**2 < 3 * a * c:
+      return True
+  if c > 0 and b**2 == 3 * a * c and b >= 0:
+      return True
+  if c > 0 and b**2 == 3 * a * c and -b >= 3 * c:
+      return True
+  if c == 0:
+      return False
+  q_p = (-2 * b + np.sqrt(4 * b**2 - 12 * a * c)) / (6 * c)
+  if c > 0 and b**2 > 3 * a * c and q_p <= 0:
+      return True
+  q_d = (-2 * b - np.sqrt(4 * b**2 - 12 * a * c)) / (6 * c)
+  if c > 0 and b**2 > 3 * a * c and q_d >= 1:
+      return True
+  if c < 0 and b**2 > 3 * a * c and q_p >= 1 and q_d <= 0:
+      return True
 
-    # Calculate discrete entropy
-    for i in range(len(histogram)):
-        p = histogram[i] / histogram_sum
-        if p != 0:
-            H += p * math.log(p)
+  return False
 
-    return -H
+def rgb_to_luminance(image):
+  return 0.2126 * image[...,0] + 0.7152 * image[...,1] + 0.0722 * image[...,2]
 
+def correct_vignetting(image):
+  """
+  Apply vignetting correction
 
-def find_parameters(cm_x, cm_y, max_distance, im):
-    """
-    Find a, b, c that could minimize the entropy of the image, given the
-    image's cenrter of mass and the distance from the image's farthest vertex
-    to center of mass
-    """
+  Parameters
+  ----------
+  image : np.arrray
+    The input image to be corrected
 
-    a = b = c = 0
-    delta = 2
-    min_H = None
+  Returns
+  -------
+  np.array
+    The corrected image (array like `image`)
+  """
+  gray_image = rgb_to_luminance(image)
 
-    # set up a explored set to optimize running time
-    explored = set()
+  (a, b, c) = (0.0, 0.0, 0.0)
+  delta = 8.0
+  h_min = log_entropy(gray_image)
+  r = compute_r_matrix(gray_image)
 
-    while delta > 1 / 256:
-        initial_tup = (a, b, c)
+  while delta > 1 / 256:
+    v_arr = np.array([(a + delta, b, c), (a - delta, b, c),
+                      (a, b + delta, c), (a, b - delta, c),
+                      (a, b, c + delta), (a, b, c - delta)])
 
-        for parameter_tup in [(a + delta, b, c), (a - delta, b, c),
-                              (a, b + delta, c), (a, b - delta, c),
-                              (a, b, c + delta), (a, b, c - delta)]:
+    print(delta)
+    
+    for v in v_arr:
 
-            if parameter_tup not in explored:
-                explored.add(parameter_tup)
+      if verify_constraints(*v):
+        h_tmp = log_entropy(gray_image * g(r, *v))
+        if h_tmp < h_min:
+          print("true")
+          h_min = h_tmp
+          (a, b, c) = v
+          delta = 16.0
 
-                if check_monotonically_increase(parameter_tup):
-                    curr_H = calc_discrete_entropy(
-                        cm_x, cm_y, max_distance, parameter_tup, im)
+    delta /= 2.0
 
-                    # if the entropy is lower than current minimum, set parameters to current ones
-                    if min_H is None or curr_H < min_H:
-                        min_H = curr_H
-                        a, b, c = parameter_tup
+  print(f"Coefficients: ({a}, {b}, {c}), Minimal entropy: {h_min}")
 
-        # if the current parameters minimize the entropy with the current delta, reduce the delta
-        if initial_tup == (a, b, c):
-            delta /= 2
+  plt.imshow(g(r, a, b, c))
+  plt.show()
 
-    return a, b, c
+  res = image * np.stack(3 * [g(r, a, b, c)], axis=2)
 
+  return np.clip(res, 0, 255).astype(np.uint8)
 
-def vignetting_correction(im):
-    """
-    Correct the vignetting of the image with the parameters that could minimize
-    the discrete entropy.
-    """
+image = plt.imread('2.jpeg')
+plt.imshow(image)
+corrected_image = correct_vignetting(image)
+plt.imshow(corrected_image)
+plt.show()
 
-    # convert rgb image to grayscale
-    imgray = cv2.transform(im, np.array([[0.2126, 0.7152, 0.0722]]))
-    row, col = imgray.shape
-
-    # calculate center of mass of the picture
-    cm_x = sum(j * imgray[i, j] for i in range(row) for j in range(col)
-               ) / sum(imgray[i, j] for i in range(row) for j in range(col))
-    cm_y = sum(i * imgray[i, j] for i in range(row) for j in range(col)
-               ) / sum(imgray[i, j] for i in range(row) for j in range(col))
-    max_distance = math.sqrt(max(
-        (vertex[0] - cm_x)**2 + (vertex[1] - cm_y)**2 for vertex in [[0, 0], [0, row], [col, 0], [col, row]]))
-
-    # if the size of the image is too large, use the reduce-size image to get parameters and apply them on the initial image to save running time
-    if col > 500:
-        ratio = col / 500
-        imgray_sm = cv2.resize(imgray, (500, round(row / ratio)))
-        a, b, c = find_parameters(
-            cm_x / ratio, cm_y / ratio, max_distance / ratio, imgray_sm)
-    else:
-        a, b, c = find_parameters(cm_x, cm_y, max_distance, imgray)
-
-    # modify the original image
-    for i in range(col):
-        for j in range(row):
-            distance = math.sqrt((i - cm_x)**2 + (j - cm_y)**2)
-            r = distance / max_distance
-            g = 1 + a * r**2 + b * r**4 + c * r**6
-
-            for k in range(3):
-                modified = im[j, i][k] * g
-
-                # if the brightness after modification is greater than 255, then set the brightness to 255
-                if modified > 255:
-                    modified = 255
-                im[j, i][k] = modified
-
-
-# read the file path from the command line
-# filename = "A/1.jpeg"
-# im = cv2.imread(filename)
-# vignetting_correction(im)
-# cv2.imwrite("result images/" + os.path.basename(filename), im)
-
-"""function looping all image from the folder and apply vignetting correction"""
-for filename in os.listdir("B"):
-    im = cv2.imread("A/" + filename)
-    print("B/" + filename)
-    # vignetting_correction(im)
-    # cv2.imwrite("result images/" + filename, im)
